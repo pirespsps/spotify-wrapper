8e65b77eabe9012d67a754336d6d12fc9d9e76b8
pirespsps <matheusknoll28@gmail.com>
2025-11-21 19:36:14 -0300

    request generica e token funcionando

--- wrapper.go
+++ wrapper.go
@@ -1,24 +1,77 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"errors"
+	"io"
 	"net/http"
+	"net/url"
 )
 
-func getClientID() error {
+func getClientID() (string, error) {
 
-	var url = "https://accounts.spotify.com/api/token"
+	var rurl = "https://accounts.spotify.com/api/token"
 
-	//fazer um body aqui e passar na request o client id e secret
+	data := url.Values{
+		"grant_type":    {"client_credentials"},
+		"client_id":     {ID},
+		"client_secret": {Secret},
+	}
 
-	req, err := http.NewRequest("POST", url, nil)
+	reader := bytes.NewBufferString(data.Encode())
+
+	req, err := http.NewRequest("POST", rurl, reader)
 	if err != nil {
-		return errors.New("requisition error")
+		return "", errors.New("client token requisition error")
 	}
 
 	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
 
-	return nil
+	response, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return "", errors.New("client token response error")
+	}
+
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		return "", errors.New("client token body error")
+	}
+
+	token := struct {
+		Token string `json:"access_token"`
+		Type  string `json:"token_type"`
+	}{}
+
+	json.Unmarshal(body, &token)
+
+	return token.Token, nil
+
+}
+
+func genericRequest(url string) ([]byte, error) {
+	token, err := getClientID()
+	if err != nil {
+		return nil, errors.New("generic request token error")
+	}
+
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, errors.New("generic request new request error")
+	}
+	req.Header.Add("Authorization", "Bearer "+token)
+
+	response, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, errors.New("generic request response error")
+	}
+
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		return nil, errors.New("generic request body error")
+	}
+
+	return body, nil
 }
 
 func makeAction() {
